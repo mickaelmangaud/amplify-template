8203fa5d29d89301b534eac18f9023fef313c7ac
Mickael Mangaud <mickael.mangaud@vitalliance.fr>
2021-08-29 17:08:16 +0200

    isLocalhost

--- src/utils/amplify.ts
+++ src/utils/amplify.ts
@@ -3,12 +3,10 @@ import config from '../aws-exports';
 export let isLocalhost = true;
 
 /* https://docs.amplify.aws/lib/auth/social/q/platform/js/#configure-auth-category */
-if (!(typeof window === 'undefined')) {
+if (typeof window !== 'undefined') {
   isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
-      // 127.0.0.1/8 is considered localhost for IPv4.
       window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
   );
 }
